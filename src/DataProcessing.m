d7e7ce46d9be44ce97281d7d8d3a9afc369a29bc
Aaron Millwee <aaronmillwee12@gmail.com>
2025-02-05 11:24:28 -0800

    matlab serial monitor

---
+++ src/DataProcessing.m
@@ -0,0 +1,34 @@
+clc
+clear
+close
+
+ESP32 = serialport('COM3',115200);
+
+i=1;
+t=0;
+tic;
+j=1;
+h=plot(NaN,NaN,'r');    % Open plot object to speed up plotting speed
+while (t <= 5)
+    
+    t=toc; % log time since loop start
+
+    check_identifier(i)=read(ESP32,1,'char');
+
+    if check_identifier(i)==':'
+        if check_identifier(i-1)=='m'
+            if check_identifier(i-2)=='c'
+                data(j,1) = t;
+                data(j,2)=read(ESP32,1,'double');
+                set(h, 'XData', data(1:j,1),'YData',data(1:j,2));
+                drawnow
+                j=j+1;
+            end
+        end
+    end
+
+    i=i+1;
+
+end
+
+clear ESP32; % Close serial port object
